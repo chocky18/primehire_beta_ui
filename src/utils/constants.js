c8ed45b9c93714b954bd9c60a7575c36522fac92
Naresh Tinnaluri <chockynaresh18@gmail.com>
2026-01-02 19:00:43 +0530

    updated mail link with updated url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,12 @@
-export const WS_URL = "wss://primehire.nirmataneurotech.com/ws";
-export const API_BASE = "https://primehire.nirmataneurotech.com";
-export const API_BASE_URL = "https://primehire.nirmataneurotech.com";
+// // export const WS_URL = "wss://primehire.nirmataneurotech.com/ws";
+// // export const API_BASE = "https://primehire.nirmataneurotech.com";
+// // export const API_BASE_URL = "https://primehire.nirmataneurotech.com";
+
+// // https://agentic.primehire.ai/
+// export const WS_URL = "wss://agentic.primehire.ai/ws";
+// export const API_BASE = "https://agentic.primehire.ai";
+// export const API_BASE_URL = "https://agentic.primehire.ai";
+export const API_BASE = import.meta.env.VITE_API_BASE;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const WS_URL = import.meta.env.VITE_WS_URL;
+export const UI_BASE = import.meta.env.VITE_UI_BASE;
