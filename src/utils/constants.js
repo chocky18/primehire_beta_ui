b15dbd4a6ac664d071b756a76b308ebf22c3e506
Naresh Tinnaluri <chockynaresh18@gmail.com>
2026-01-04 21:33:23 +0530

    updated constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -7,12 +7,12 @@
 // export const API_BASE = "https://agentic.primehire.ai";
 // export const API_BASE_URL = "https://agentic.primehire.ai";
 
-// export const API_BASE = "https://agentic.primehire.ai/api";
-// export const API_BASE_URL = "https://agentic.primehire.ai/api";
-// export const UI_BASE = "https://agentic.primehire.ai"
-// export const WS_URL = "wss://agentic.primehire.ai/ws";
+export const API_BASE = "https://agentic.primehire.ai/api";
+export const API_BASE_URL = "https://agentic.primehire.ai/api";
+export const UI_BASE = "https://agentic.primehire.ai"
+export const WS_URL = "wss://agentic.primehire.ai/ws";
 
-export const WS_URL = "wss://primehire.nirmataneurotech.com/ws";
-export const API_BASE = "https://primehire.nirmataneurotech.com";
-export const API_BASE_URL = "https://primehire.nirmataneurotech.com";
-export const UI_BASE = "https://primehire-beta-ui.vercel.app"
+// export const WS_URL = "wss://primehire.nirmataneurotech.com/ws";
+// export const API_BASE = "https://primehire.nirmataneurotech.com";
+// export const API_BASE_URL = "https://primehire.nirmataneurotech.com";
+// export const UI_BASE = "https://primehire-beta-ui.vercel.app"
