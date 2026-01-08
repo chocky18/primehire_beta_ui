cdca94e6ff7ae43fb105c89fdca147acd41573a5
Naresh Tinnaluri <chockynaresh18@gmail.com>
2026-01-08 15:32:17 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,42 @@
+// // import React from "react";
+// // import ReactDOM from "react-dom/client";
+// // import { BrowserRouter, Routes, Route } from "react-router-dom";
+// // import App from "./App";
+// // import "./global.css";
+
+// // // import Apply from "./pages/Apply";
+
+
+// // ReactDOM.createRoot(document.getElementById("root")).render(
+// //     <React.StrictMode>
+// //         <BrowserRouter>
+// //             <Routes>
+// //                 <Route path="/" element={<App />} />
+// //                 {/* <Route path="/apply" element={<Apply />} /> */}
+// //             </Routes>
+// //         </BrowserRouter>
+// //     </React.StrictMode>
+// // );
 // import React from "react";
 // import ReactDOM from "react-dom/client";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import App from "./App";
-// import "./global.css";
+// import App from "./App.jsx";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
-// // import Apply from "./pages/Apply";
 
+// import "@/styles/global.css";
+// // import "@/styles/chat-layout.css";
 
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //     <React.StrictMode>
-//         <BrowserRouter>
-//             <Routes>
-//                 <Route path="/" element={<App />} />
-//                 {/* <Route path="/apply" element={<Apply />} /> */}
-//             </Routes>
-//         </BrowserRouter>
+//         <App />
 //     </React.StrictMode>
 // );
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-
 import "@/styles/global.css";
-// import "@/styles/chat-layout.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+    <App />
 );
